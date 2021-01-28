fba0df4f5f85d8acf2d06a4bb2829111a6aeca76
quentin roux quentin.roux@hotmail.fr <quentin.roux@hotmail.fr>
2021-01-28 23:56:25 +0100

    bet queries populated working

--- api/bets/controllers/bets.js
+++ api/bets/controllers/bets.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    try {
+      entities = await strapi.query("bets").find(ctx.query, {
+        path: "match",
+        populate: [{ path: "home" }, { path: "visitor" }],
+      });
+    } catch (err) {
+      return err;
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.bets })
+    );
+  },
+};

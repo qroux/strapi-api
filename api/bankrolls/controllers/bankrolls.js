625ff31c5fdcf1a1d19ff2366802a08f17ae9bb5
Quentin Roux <quentin.roux@hotmail.fr>
2021-02-09 16:19:52 +0100

    GET /bankrolls populated with bets

--- api/bankrolls/controllers/bankrolls.js
+++ api/bankrolls/controllers/bankrolls.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async find(ctx) {
+    let entities;
+
+    try {
+      entities = await strapi.query("bankrolls").find(ctx.query, {
+        path: "positions",
+        populate: [{ path: "bet" }],
+      });
+    } catch (err) {
+      return err;
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.bankrolls })
+    );
+  },
+};

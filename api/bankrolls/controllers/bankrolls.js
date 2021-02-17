919d0e27039c2dc070376317c41c83a83400348b
qroux <quentin.roux@hotmail.fr>
2021-02-17 22:10:37 +0100

    Deep populate syntax updated on GET /bankrolls to add Match relation

--- api/bankrolls/controllers/bankrolls.js
+++ api/bankrolls/controllers/bankrolls.js
@@ -11,10 +11,11 @@ module.exports = {
   async find(ctx) {
     let entities;
 
+    // DEEP POPULATE SYNTAX
     try {
       entities = await strapi.query("bankrolls").find(ctx.query, {
         path: "positions",
-        populate: [{ path: "bet" }],
+        populate: [{ path: "bet", populate: [{ path: "match" }] }],
       });
     } catch (err) {
       return err;

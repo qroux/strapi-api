15266b7e220c1b047913c7b35a1a890df1321386
qroux <quentin.roux@hotmail.fr>
2021-03-18 19:42:42 +0100

    TEST position CRUD + helper working + Full model test coverage done

--- tests/helpers/position.js
+++ tests/helpers/position.js
@@ -1,3 +1,29 @@
-const buildPositionParms = async () => {};
+const { userInstance } = require("./user");
+const { buildBankroll } = require("./bankroll");
+const { buildBet } = require("./bet");
 
-module.exports = {};
+const getBankrollId = async () => {
+  return await buildBankroll();
+};
+
+const getBetId = async () => {
+  return await buildBet();
+};
+
+const buildPositionParams = async () => {
+  const userId = userInstance.userOBJ.id;
+  const bankroll = await getBankrollId();
+  const bet = await getBetId();
+
+  const mockPositionData = {
+    users_permissions_user: userId,
+    bankroll: bankroll.id,
+    bet: bet.id,
+    value: 20,
+    status: "Attente",
+  };
+
+  return mockPositionData;
+};
+
+module.exports = { buildPositionParams };
--- tests/positions/index.js
+++ tests/positions/index.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const { userInstance } = require("../helpers/user");
 const mongoose = require("mongoose");
-const
+const { buildPositionParams } = require("../helpers/position");
 
 // POSITIONS COLLECTION PARAMS
 // @@users_permissions_user: Reference id => User model
@@ -11,82 +11,94 @@ const
 // @@outcome!: Decimal => default: 0
 // @@status!: Enum => ["Attente","Gagnant","Perdu"] => default: "Attente"
 
-const mockPositionData = {
-  users_permissions_user: userInstance.userOBJ.id,
-  bankroll: "",
-  bet: "",
-  value: 20,
-};
+// const { id } = await buildBankroll();
+
+// const positionParams = {
+//   users_permissions_user: userInstance.userOBJ.id,
+//   bankroll: buildBankroll().id,
+//   bet: "",
+//   value: 20,
+// };
 
 let positionID = undefined;
 
 describe("Position CRUD", () => {
   it("CREATE position if all required params provided", async (done) => {
+    const positionParams = await buildPositionParams();
+
     const response = await request(strapi.server)
       .post("/positions")
       .set("accept", "application/json")
       .set("Content-Type", "application/json")
-      .set("Authorization", `Bearer ${userJWT}`)
-      .send(mockPositionData)
+      .set("Authorization", `Bearer ${userInstance.JWT}`)
+      .send(positionParams)
       .expect("Content-Type", /json/)
       .expect(200)
       .then((res) => {
-        expect(res.body.type).toEqual(mockPositionData.type);
-        expect(res.body.match.id).toEqual(mockPositionData.match); // match reference populated
-        expect(res.body.odds).toEqual(mockPositionData.odds);
-        expect(res.body.bookmaker).toEqual(mockPositionData.bookmaker);
-        expect(res.body.status).toEqual(mockPositionData.status);
+        expect(res.body.type).toEqual(positionParams.type);
+        expect(res.body.bet.id).toEqual(positionParams.bet); // match reference populated
+        expect(res.body.odds).toEqual(positionParams.odds);
+        expect(res.body.bookmaker).toEqual(positionParams.bookmaker);
+        expect(res.body.status).toEqual(positionParams.status);
         positionID = res.body.id;
       });
     expect(positionID).toBeDefined();
     done();
   });
-  // it("GET bet at id", async (done) => {
-  //   await request(strapi.server)
-  //     .get(`/positions/${positionID}`)
-  //     .set("Authorization", `Bearer ${userJWT}`)
-  //     .send()
-  //     .expect("Content-Type", /json/)
-  //     .expect(200)
-  //     .then((res) => {
-  //       expect(res.body.type).toEqual(mockPositionData.type);
-  //       expect(mongoose.Types.ObjectId.isValid(res.body.match.id)).toEqual(
-  //         true
-  //       ); // reference is automatically populated by strapi
-  //       expect(res.body.odds).toEqual(mockPositionData.odds);
-  //       expect(res.body.bookmaker).toEqual(mockPositionData.bookmaker);
-  //       expect(res.body.status).toEqual(mockPositionData.status);
-  //     });
-  //   done();
-  // });
-  // it("UPDATE bet at id", async (done) => {
-  //   const updatedType = "MODIFIED MOCKED gagne par plus de deux buts d'écart";
-  //   await request(strapi.server)
-  //     .put(`/positions/${positionID}`)
-  //     .set("accept", "application/json")
-  //     .set("Content-Type", "application/json")
-  //     .set("Authorization", `Bearer ${userJWT}`)
-  //     .send({ ...mockPositionData, type: updatedType })
-  //     .expect("Content-Type", /json/)
-  //     .expect(200)
-  //     .then((res) => {
-  //       expect(res.body.type).toEqual(updatedType);
-  //       expect(mongoose.Types.ObjectId.isValid(res.body.match.id)).toEqual(
-  //         true
-  //       );
-  //       expect(res.body.odds).toEqual(mockPositionData.odds);
-  //       expect(res.body.bookmaker).toEqual(mockPositionData.bookmaker);
-  //       expect(res.body.status).toEqual(mockPositionData.status);
-  //     });
-  //   done();
-  // });
-  // it("DELETE bet at id", async (done) => {
-  //   await request(strapi.server)
-  //     .delete(`/positions/${positionID}`)
-  //     .set("Authorization", `Bearer ${userJWT}`)
-  //     .send()
-  //     .expect("Content-Type", /json/)
-  //     .expect(200);
-  //   done();
-  // });
+  it("GET bet at id", async (done) => {
+    const positionParams = await buildPositionParams();
+
+    await request(strapi.server)
+      .get(`/positions/${positionID}`)
+      .set("Authorization", `Bearer ${userInstance.JWT}`)
+      .send()
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.type).toEqual(positionParams.type);
+        expect(res.body.odds).toEqual(positionParams.odds);
+        expect(res.body.bookmaker).toEqual(positionParams.bookmaker);
+        expect(res.body.status).toEqual(positionParams.status);
+      });
+    done();
+  });
+  it("UPDATE bet at id", async (done) => {
+    const positionParams = await buildPositionParams();
+    const updatedValue = 666;
+
+    await request(strapi.server)
+      .put(`/positions/${positionID}`)
+      .set("accept", "application/json")
+      .set("Content-Type", "application/json")
+      .set("Authorization", `Bearer ${userInstance.JWT}`)
+      .send({ ...positionParams, value: updatedValue })
+      .expect("Content-Type", /json/)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.value).toEqual(updatedValue);
+        expect(res.body.odds).toEqual(positionParams.odds);
+        expect(res.body.bookmaker).toEqual(positionParams.bookmaker);
+        expect(res.body.status).toEqual(positionParams.status);
+      });
+    done();
+  });
+  it("DELETE bet at id", async (done) => {
+    await request(strapi.server)
+      .delete(`/positions/${positionID}`)
+      .set("Authorization", `Bearer ${userInstance.JWT}`)
+      .send()
+      .expect("Content-Type", /json/)
+      .expect(200);
+    done();
+  });
+});
+
+describe("Position help", () => {
+  it("Position helper", async (done) => {
+    const positionParams = await buildPositionParams();
+
+    expect(positionParams).toBeDefined();
+
+    done();
+  });
 });
